0a92bf9870376cc86617ec134f96a3b358eb38d2
fcd <git@fcd.dev>
2021-03-05 17:13:52 +0000

    don't have to think different anymore

--- fla.sh
+++ fla.sh
@@ -6,4 +6,4 @@ ARDUINO_VERSION=${ARDUINO_VERSION:-1.8.11}
 export ARDUINO_PATH="${HOME}/z/dy/arduino-${ARDUINO_VERSION}"
 export SKETCHBOOK_DIR="${HOME}/z/dy/arduino-${ARDUINO_VERSION}"
 
-make $@
+make flash $@
